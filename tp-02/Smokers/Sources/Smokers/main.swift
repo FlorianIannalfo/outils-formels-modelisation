a963dd1fc06143ba630245fa8519d00bcede268f
Florian Iannalfo <32091770+FlorianIannalfo@users.noreply.github.com>
2017-10-26 22:09:49 +0200

    Update main.swift

--- tp-02/Smokers/Sources/Smokers/main.swift
+++ tp-02/Smokers/Sources/Smokers/main.swift
@@ -24,5 +24,12 @@ let initialMarking: PTMarking = [r: 1, p: 0, t: 0, m: 0, w1: 1, s1: 0, w2: 1, s2
 
 // Create the marking graph (if possible).
 if let markingGraph = model.markingGraph(from: initialMarking) {
-    // Write here the code necessary to answer questions of Exercise 4.
+// Write here the code necessary to answer questions of Exercise 4.
+//
+print("1.)")
+print(model.StatesN(input: markingGraph))
+print("2.)")
+print(model.TwoSmk(imput: markingGraph))
+print("3.)")
+print(model.DuplicateObj(input: markingGraph))
 }
